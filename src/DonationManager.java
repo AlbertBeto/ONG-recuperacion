15ad6d5fa88d5b6e009cc9bd02f6f97540952720
Beto <apbaleyto@gmail.com>
2023-04-24 02:38:57 +0200

    Solucionado lo de la impresión de mayor de edad del DonationManager. Falta ver lo de la impresión formato USA de la fecha de nacimiento.

--- src/DonationManager.java
+++ src/DonationManager.java
@@ -8,10 +8,11 @@ public class DonationManager {
        }else {
            System.out.println("La donación no ha sido realizada por la persona solicitada.");
        }
-
+//Hago un segundo IF para confirmar si la persona es mayor de edad. Para evitar impresiones si no es la misma persona descarto
+//el ELSE y realizo un else if que vuelve a confirmar si es la misma persona y si sumandole 18 la fecha va mas alla de hoy.
         if (donacion.getDocument().equals(persona.getDocument())&&persona.getBirthday().plusYears(18).isBefore(LocalDate.now())) {
             System.out.println("La persona es mayor de edad.");
-        }else{
+        } else if (donacion.getDocument().equals(persona.getDocument())&&persona.getBirthday().plusYears(18).isAfter(LocalDate.now())) {
             System.out.println("La persona no es mayor de edad.");
         }
     }
